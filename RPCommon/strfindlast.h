982d5ec0ee78cc1bd51d7264377fbf25e147804f
Anzo <antonr@live.dk>
2021-07-14 15:24:06 +0200

    fixed small oversight

--- RPCommon/strfindlast.h
+++ RPCommon/strfindlast.h
@@ -5,7 +5,7 @@ size_t StringFindLast(char c, std::string s)
     size_t tmp = 0;
     //Keep the length so we don't have to get it from the string every loop
     size_t len = s.length();
-    for(size_t i = 0; i < s.length(); i++)
+    for(size_t i = 0; i < len; i++)
     {
         if(s.at(len-1-i) == c)
         {
