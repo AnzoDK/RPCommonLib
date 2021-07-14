abe49f1d815265937fc7f0662d0d842bb8b97c34
Anzo <antonr@live.dk>
2021-07-14 15:25:00 +0200

    fixed another oversight

--- RPCommon/strreverse.h
+++ RPCommon/strreverse.h
@@ -3,9 +3,10 @@
 static std::string ReverseString(std::string s)
 {
     std::string tmp = "";
-    for(size_t i = 0; i < s.length(); i++)
+    size_t len = s.length();
+    for(size_t i = 0; i < len; i++)
     {
-        tmp+=s.at(s.length()-i-1);
+        tmp+=s.at(len-i-1);
     }
     return tmp;
 }
