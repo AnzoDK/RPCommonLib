8a5a480de8b3534748c664983365ed8010fb0510
AnzoDK <antonr@live.dk>
2021-07-15 12:49:11 +0200

    Added Cstrcmp as to not include <stdio.h> or <string>

--- RPCommon/RPCommon.h
+++ RPCommon/RPCommon.h
@@ -1,3 +1,4 @@
 #pragma once
 #include "strreverse.h"
 #include "strfindlast.h"
+#include "strcomp.h"
---
+++ RPCommon/strcomp.h
@@ -0,0 +1,19 @@
+//C String comapre
+//expecting nulltermination unless "no-null" is specified, in that case the length of the string will need to be provided
+bool Cstrcmp(const char* s1, const char* s2)
+{
+    uint64_t c = 0;
+    while(static_cast<unsigned char>(s1[c]) != 0x0 && static_cast<unsigned char>(s2[c]) != 0x0)
+    {
+        if(s1[c] != s2[c])
+        {
+            return false;
+        }
+        c++;
+    }
+    if(static_cast<unsigned char>(s1[c]) == 0x0 && static_cast<unsigned char>(s2[c]) == 0x0)
+    {
+        return true;
+    }
+    return false;
+}
