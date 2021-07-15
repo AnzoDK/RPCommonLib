eb0acfe09165b5e4c5d899943c432087ef37de9d
AnzoDK <antonr@live.dk>
2021-07-15 21:30:17 +0200

    Fixed bug in StringFindLast and Added new vararity

--- RPCommon/strfindlast.h
+++ RPCommon/strfindlast.h
@@ -9,7 +9,23 @@ size_t StringFindLast(char c, std::string s)
     {
         if(s.at(len-1-i) == c)
         {
-            tmp = len-1-1;
+            tmp = len-i-1;
+            return tmp;
+        }
+    }
+    return std::string::npos;
+}
+size_t StringFindLast(std::string search, std::string s)
+{
+    size_t tmp = 0;
+    //Keep the length so we don't have to get it from the string every loop
+    size_t len = s.length();
+    size_t searchLen = search.length();
+    for(size_t i = 0; i < len; i++)
+    {
+        if(s.substr(len-i-1-searchLen,searchLen) == search)
+        {
+            tmp = len-i-1-searchLen;
             return tmp;
         }
     }
