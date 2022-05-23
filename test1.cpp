07cd409f26f6fc844bc34e8d913adcb12c5a6cbe
Anzo <antonr@live.dk>
2022-05-23 23:44:45 +0200

    Added new test to GetByteFromTable

--- test1.cpp
+++ test1.cpp
@@ -88,6 +88,25 @@ bool PerformTestCstrcmp()
     
     std::cout << TERMINAL_COLOR_GREEN << "Passed!" << TERMINAL_COLOR_RESET << std::endl;
     
+    std::cout << "Testing ReverseHexTable..." << std::endl;
+    std::string hexText = "60";
+    unsigned char hexByte = 0x60;
+    std::cout << "Comparing " << TERMINAL_COLOR_BLUE << hexText << TERMINAL_COLOR_RESET << " to " << TERMINAL_COLOR_BLUE << hexByte << TERMINAL_COLOR_RESET << std::endl;
+    
+    if(GetByteFromTable(hexText) != hexByte)
+    {
+        std::cout << TERMINAL_COLOR_RED << " String to byte FAILED TO CONVERT" << TERMINAL_COLOR_RESET << std::endl;
+        return false;
+    }
+    hexText+='0';
+    if(GetByteFromTable(hexText) != (unsigned char)-1)
+    {
+        std::cout << TERMINAL_COLOR_RED << " String to byte FAILED TO DETECT ERRORS" << TERMINAL_COLOR_RESET << std::endl;
+        return false;
+    }
+    
+    std::cout << TERMINAL_COLOR_GREEN << "Passed!" << TERMINAL_COLOR_RESET << std::endl;
+    
     return true;
 }
 
